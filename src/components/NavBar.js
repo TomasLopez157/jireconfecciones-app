7fb2fec7c59b340cd5a5f150f45fdc3707e665d2
Tomas.Lopez <tomaslopez15797@gmail.com>
2021-12-13 21:56:04 -0300

    parte3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 function NavBar(){
     return (
-    <nav className='menu'>
+        <header>
+        <nav className='menu'>
         <a href='inicio'>inicio</a>
         <a href='ropa'>ropa</a>
-        <a href='nosotros'>nosotros</a>
+        <a href='nosotros'>nosotros</a> 
                        
     </nav>
+        </header>
+
     );
 }
 
