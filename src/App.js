95e676586580ed78fd375ef548ebf8403093e146
Tomas.Lopez <tomaslopez15797@gmail.com>
2021-12-15 22:17:49 -0300

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@
 import React from 'react';
 import './App2.css';
 import NavBar from './components/NavBar';
-//import Shell from './components/Shell';
+
 
 function App() {
   return (
     <React.Fragment className="app">
        
       <NavBar/>
-    
     </React.Fragment>
   );
   }
