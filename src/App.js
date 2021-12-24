ac70fc11750d9944577b79d60ce051f4bdee6c32
Tomas.Lopez <tomaslopez15797@gmail.com>
2021-12-24 03:01:06 -0300

    yyy

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './App2.css';
 import NavBar from './components/NavBar';
 import navBar from './components/navBar.css';
-import itemListConteiner from './itemListCoonteiner.JS';
 import CartWidget from './components/CartWidget1';
+import itemListConteiner from './components/itemListCoonteiner.JS';
 
 
 
